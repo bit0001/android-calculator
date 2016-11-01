20b519cbf2d3eefda5077059ce39d2922aa378cd
m <lmiguelvargasf@gmail.com>
2016-11-01 00:01:12 -0500

    refactor: Move description logic

--- app/src/main/java/com/mathsistor/m/calculator/CalculatorBrain.java
+++ app/src/main/java/com/mathsistor/m/calculator/CalculatorBrain.java
@@ -2,11 +2,8 @@ package com.mathsistor.m.calculator;
 
 import android.annotation.TargetApi;
 import android.os.Build;
-import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.HashMap;
 
 /**
@@ -17,22 +14,18 @@ public class CalculatorBrain {
 
     private double accumulator;
     private PendingBinaryOperationInfo pending;
-    private String description;
-    private String previousAppend;
-    private String baseDescription;
+    private OperationDescription description;
     private HashMap<String, Operation> operations;
-    private HashMap<String, String> symbols;
 
     CalculatorBrain() {
-        this.description = "";
         operations = Util.OPERATIONS;
-        symbols = Util.SYMBOLS;
+        description = new OperationDescription();
     }
 
     public void setOperand(double operand) {
         accumulator = operand;
         if (!isPartialResult()) {
-            this.description = "";
+            description = new OperationDescription();
         }
     }
 
@@ -42,11 +35,12 @@ public class CalculatorBrain {
         Operation operation = operations.get(symbol);
 
         if (operation != null) {
-            setDescription(symbol);
+            description.update(symbol, accumulator, isPartialResult());
             computeResult(operation);
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     private void computeResult(Operation operation) {
         switch (operation) {
             case PI_CONSTANT:
@@ -85,164 +79,6 @@ public class CalculatorBrain {
         }
     }
 
-    private void setDescription(String operator) {
-        Operation operation = operations.get(operator);
-
-        switch (operation) {
-            case UNARY_NEGATIVE:
-                if (isPartialResult()) {
-                    if (previousAppend != null) {
-                        previousAppend = "(" + "-" + previousAppend + ")";
-                        description = baseDescription + previousAppend;
-                    } else {
-                        baseDescription = description;
-                        previousAppend = "(" + "-" + getAccumulatorString() + ")";
-                        description += previousAppend;
-                    }
-                } else {
-                    if (!description.equals("")) {
-                        description = "-" + "(" + description + ")";
-                    }
-                }
-                break;
-            case SQUARE_ROOT:
-            case CUBE_ROOT:
-            case SIN:
-            case COS:
-            case TAN:
-            case LOG10:
-            case LN:
-                if (isPartialResult()) {
-                    if (previousAppend != null) {
-                        previousAppend = operator + "(" + previousAppend + ")";
-                        description = baseDescription + previousAppend;
-                    } else {
-                        baseDescription = description;
-                        previousAppend = operator + "(" + getAccumulatorString() + ")";
-                        description += previousAppend;
-                    }
-                } else {
-                    if (description.equals("")) {
-                        description = operator + "(" + getAccumulatorString() + ")";
-                    } else {
-                        description = operator + "(" + description + ")";
-                    }
-                }
-                break;
-            case X_POWER_MINUS_1:
-            case SQUARE:
-            case CUBE:
-                if (isPartialResult()) {
-                    if (previousAppend != null) {
-                        previousAppend = "(" + previousAppend + ")" + getSymbol(operator);
-                        description = baseDescription + previousAppend;
-                    } else {
-                        baseDescription = description;
-                        previousAppend = "(" + getAccumulatorString() + ")" + getSymbol(operator);
-                        description += previousAppend;
-                    }
-                } else {
-                    if (description.equals("")) {
-                        description = "(" + getAccumulatorString() + ")" + getSymbol(operator);
-                    } else {
-                        description = "(" + description + ")" + getSymbol(operator);
-                    }
-                }
-                break;
-            case TEN_POWER:
-            case EXP:
-                if (isPartialResult()) {
-                    if (previousAppend != null) {
-                        previousAppend = getSymbol(operator) + "(" + previousAppend + ")";
-                        description = baseDescription + previousAppend;
-                    } else {
-                        baseDescription = description;
-                        previousAppend = getSymbol(operator) + "(" + getAccumulatorString() + ")";
-                        description += previousAppend;
-                    }
-                } else {
-                    if (description.equals("")) {
-                        description = getSymbol(operator) + "(" + getAccumulatorString() + ")";
-                    } else {
-                        description = getSymbol(operator) + "(" + description + ")";
-                    }
-                }
-                break;
-            case ADDITION:
-            case SUBTRACTION:
-            case MULTIPLICATION:
-            case DIVISION:
-                if (isPartialResult()) {
-                    if (previousAppend == null) {
-                        description += getAccumulatorString() + operator;
-                    } else {
-                        description += operator;
-                        previousAppend = null;
-                    }
-                } else {
-                    if (description.equals("")) {
-                        description = getAccumulatorString() + operator;
-                    } else {
-                        description += operator;
-                    }
-                }
-                break;
-            case N_POWER:
-                if (isPartialResult()) {
-                    if (previousAppend == null) {
-                        description = "(" + description +  getAccumulatorString() + ")^";
-                    } else {
-                        description = "(" + description + ")^";
-                        previousAppend = null;
-                    }
-                } else {
-                    if (description.equals("")) {
-                        description = "(" + getAccumulatorString() + ")^";
-                    } else {
-                        description = "(" + description + ")^";
-                    }
-                }
-                break;
-            case EQUALS:
-                if (!isPartialResult()) {
-                    return;
-                }
-
-                if (previousAppend == null) {
-                    description += getAccumulatorString();
-                }
-                previousAppend = null;
-                break;
-        }
-    }
-
-    @NonNull
-    private String getSymbol(String operator) {
-        return symbols.get(operator);
-    }
-
-    @NonNull
-    private String getAccumulatorString() {
-        String accumulatorString;
-        if (accumulator == Math.PI) {
-            accumulatorString = "\u03c0";
-        } else if (accumulator == Math.E) {
-            accumulatorString = "e";
-        } else {
-            double decimalPart = accumulator - Math.floor(accumulator);
-
-            if (Math.abs(decimalPart) < 0.000001) {
-                return String.valueOf((int) accumulator);
-            }
-
-            NumberFormat formatter = new DecimalFormat("#0.######");
-
-            accumulatorString = formatter.format(accumulator);
-        }
-
-        return accumulatorString;
-    }
-
     @RequiresApi(api = Build.VERSION_CODES.N)
     private void executePendingBinaryOperation() {
         if (pending != null) {
@@ -256,7 +92,7 @@ public class CalculatorBrain {
     }
 
     public String getDescription() {
-        return description;
+        return description.getDescription();
     }
 
     public boolean isPartialResult() {
--- app/src/main/java/com/mathsistor/m/calculator/OperationDescription.java
+++ app/src/main/java/com/mathsistor/m/calculator/OperationDescription.java
@@ -1,9 +1,178 @@
 package com.mathsistor.m.calculator;
 
+import android.support.annotation.NonNull;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.HashMap;
+
 /**
  * Created by m on 10/31/2016.
  */
 
 public class OperationDescription {
+    private String previousAppend;
+    private String baseDescription;
+    private String description;
+    private HashMap<String, Operation> operations;
+    private HashMap<String, String> symbols;
+
+    public OperationDescription() {
+        operations = Util.OPERATIONS;
+        symbols = Util.SYMBOLS;
+        description = "";
+    }
+
+    public void update(String operator, Double accumulator, boolean isPartialResult) {
+        Operation operation = operations.get(operator);
+
+        switch (operation) {
+            case UNARY_NEGATIVE:
+                if (isPartialResult) {
+                    if (previousAppend != null) {
+                        previousAppend = "(" + "-" + previousAppend + ")";
+                        description = baseDescription + previousAppend;
+                    } else {
+                        baseDescription = description;
+                        previousAppend = "(" + "-" + getAccumulatorString(accumulator) + ")";
+                        description += previousAppend;
+                    }
+                } else {
+                    if (!description.equals("")) {
+                        description = "-" + "(" + description + ")";
+                    }
+                }
+                break;
+            case SQUARE_ROOT:
+            case CUBE_ROOT:
+            case SIN:
+            case COS:
+            case TAN:
+            case LOG10:
+            case LN:
+                if (isPartialResult) {
+                    if (previousAppend != null) {
+                        previousAppend = operator + "(" + previousAppend + ")";
+                        description = baseDescription + previousAppend;
+                    } else {
+                        baseDescription = description;
+                        previousAppend = operator + "(" + getAccumulatorString(accumulator) + ")";
+                        description += previousAppend;
+                    }
+                } else {
+                    if (description.equals("")) {
+                        description = operator + "(" + getAccumulatorString(accumulator) + ")";
+                    } else {
+                        description = operator + "(" + description + ")";
+                    }
+                }
+                break;
+            case X_POWER_MINUS_1:
+            case SQUARE:
+            case CUBE:
+                if (isPartialResult) {
+                    if (previousAppend != null) {
+                        previousAppend = "(" + previousAppend + ")" + getSymbol(operator);
+                        description = baseDescription + previousAppend;
+                    } else {
+                        baseDescription = description;
+                        previousAppend = "(" + getAccumulatorString(accumulator) + ")" + getSymbol(operator);
+                        description += previousAppend;
+                    }
+                } else {
+                    if (description.equals("")) {
+                        description = "(" + getAccumulatorString(accumulator) + ")" + getSymbol(operator);
+                    } else {
+                        description = "(" + description + ")" + getSymbol(operator);
+                    }
+                }
+                break;
+            case TEN_POWER:
+            case EXP:
+                if (isPartialResult) {
+                    if (previousAppend != null) {
+                        previousAppend = getSymbol(operator) + "(" + previousAppend + ")";
+                        description = baseDescription + previousAppend;
+                    } else {
+                        baseDescription = description;
+                        previousAppend = getSymbol(operator) + "(" + getAccumulatorString(accumulator) + ")";
+                        description += previousAppend;
+                    }
+                } else {
+                    if (description.equals("")) {
+                        description = getSymbol(operator) + "(" + getAccumulatorString(accumulator) + ")";
+                    } else {
+                        description = getSymbol(operator) + "(" + description + ")";
+                    }
+                }
+                break;
+            case ADDITION:
+            case SUBTRACTION:
+            case MULTIPLICATION:
+            case DIVISION:
+                if (isPartialResult) {
+                    if (previousAppend == null) {
+                        description += getAccumulatorString(accumulator) + operator;
+                    } else {
+                        description += operator;
+                        previousAppend = null;
+                    }
+                } else {
+                    if (description.equals("")) {
+                        description = getAccumulatorString(accumulator) + operator;
+                    } else {
+                        description += operator;
+                    }
+                }
+                break;
+            case N_POWER:
+                if (isPartialResult) {
+                    if (previousAppend == null) {
+                        description = "(" + description +  getAccumulatorString(accumulator) + ")^";
+                    } else {
+                        description = "(" + description + ")^";
+                        previousAppend = null;
+                    }
+                } else {
+                    if (description.equals("")) {
+                        description = "(" + getAccumulatorString(accumulator) + ")^";
+                    } else {
+                        description = "(" + description + ")^";
+                    }
+                }
+                break;
+            case EQUALS:
+                if (!isPartialResult) {
+                    return;
+                }
+
+                if (previousAppend == null) {
+                    description += getAccumulatorString(accumulator);
+                }
+                previousAppend = null;
+                break;
+        }
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @NonNull
+    private String getAccumulatorString(Double accumulator) {
+        if (accumulator == Math.PI) {
+            return  "\u03c0";
+        } else if (accumulator == Math.E) {
+            return  "e";
+        } else {
+            NumberFormat formatter = new DecimalFormat("#0.######");
+            return formatter.format(accumulator);
+        }
+    }
+
+    @NonNull
+    private String getSymbol(String operator) {
+        return symbols.get(operator);
+    }
 
 }
